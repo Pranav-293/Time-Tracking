9125e3159e8e1e81b18a726c15e13f7f8790355b
Pranav Gupta <Pranav.19142@davietjal.org>
2021-07-19 23:09:18 +0530

    made form widget for jobs page

--- lib/home/jobs/add_job_page.dart
+++ lib/home/jobs/add_job_page.dart
@@ -16,6 +16,50 @@ class _AddJobPageState extends State<AddJobPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Add Job"),),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
+}
+
+Widget _buildContent() {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Card(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child:_buildForm(),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _buildForm() {
+  return Form(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: _formItems(),
+    ),
+  );
+}
+
+List<Widget> _formItems() {
+  return [
+    TextFormField(
+      decoration: InputDecoration(
+        labelText: "Job name",
+      ),
+    ),
+    TextFormField(
+      decoration: InputDecoration(
+        labelText: "Rate per Hour",
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        signed: false,
+        decimal: false,
+      ),
+    ),
+  ];
 }
